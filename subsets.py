66f33f3329179c874589879b13888c25eb4a3a09
QuentenW <quenten.welch1@ucalgary.ca>
2025-01-25 15:15:31 -0600

    solution

--- subsets.py
+++ subsets.py
@@ -1,2 +1,14 @@
 #Generate all subsets of a given list.
 
+def subsets(nums):
+    result = []
+    def backtrack(start, path):
+        result.append(path)
+        for i in range(start, len(nums)):
+            backtrack(i + 1, path + [nums[i]])
+    backtrack(0, [])
+    return result
+
+# Example
+nums = [1, 2, 3]
+print("Subsets:", subsets(nums))
